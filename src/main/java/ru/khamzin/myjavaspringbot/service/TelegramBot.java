fdcbd0f4be70389c5e2831fc487d78522f8d095d
thebestWasabi <openpw@yandex.ru>
2023-06-01 02:16:43 +0300

    Добавлять первую команду для бота "/start"
    В методе onUpdateReceived в switch добавил первую команду. Проверил - все работает.

--- src/main/java/ru/khamzin/myjavaspringbot/service/TelegramBot.java
+++ src/main/java/ru/khamzin/myjavaspringbot/service/TelegramBot.java
@@ -1,24 +1,60 @@
 package ru.khamzin.myjavaspringbot.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.khamzin.myjavaspringbot.config.BotConfig;
 
 @Component
+@RequiredArgsConstructor
 public class TelegramBot extends TelegramLongPollingBot {
+
+    private final BotConfig botConfig;
 
     @Override
     public String getBotUsername() {
-        return null;
+        return botConfig.getBotName();
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return botConfig.getToken();
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
 
+
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendMessage(chatId, "Эта команда не поддерживается");
+            }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+        String answer = "Hi, " + name + ", nice to meet you!";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
